f504a4a29567dc8036aa967ae59364c27a8b064e
kanmbieydebra <adepejukanmbi@gmail.com>
2022-10-13 23:39:07 +0100

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main-print a string without prinf and put functions
+  *Return:1 if success.
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
