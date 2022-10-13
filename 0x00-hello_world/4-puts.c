8387971ae7131c57fb0b89f533c46d71b990f77e
kanmbieydebra <adepejukanmbi@gmail.com>
2022-10-13 22:53:01 +0100

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main-prints a string
+  *Return:0 if no errors.
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+  *main-prints out a string
+  *Return:0 if no error.
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
