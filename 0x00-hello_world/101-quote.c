d405128dd67ce94b48904c0c1eea869702c84e92
kanmbieydebra <adepejukanmbi@gmail.com>
2022-10-13 23:46:20 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   *main-print a string without prinf and put functions
   *Return:1 if success.
   */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
