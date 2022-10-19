dc41db98da91c9d7e9c57e679448e22ccb035af0
kanmbieydebra <adepejukanmbi@gmail.com>
2022-10-20 00:11:15 +0100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _abs - returns absolute value
+  *@n: interger value
+  * Return: 1 if character is lowercase, 0 otherwise.
+  */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
