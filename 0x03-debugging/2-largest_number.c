a4009fff00032322ee0153dccce178a52b1e24d0
kanmbieydebra <adepejukanmbi@gmail.com>
2022-10-20 18:27:06 +0100

    v

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
-	{
-		if (b > c)
-			largest = a;
-	}
-	else if (b > a)
-	{
-		if (a > c)
-			largest = b;
-	}
+	if ((a > b) && (b > c))
+		largest == a;
+	else if ((b > a) && (a > c))
+		largest == b;
 	else
-	{
-		largest = c;
-	}
+		largest == c;
 	return (largest);
 }
